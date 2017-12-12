a17203eb0587d3f7054363b7846600a0d8d92272
ORCHARD\Karl.Johnson <karl.johnson@orchard.com.au>
2017-12-12 17:25:39 +1100

    CSharp6 - Features Part 1

---
+++ CSharp/CSharp6Features/AutoProperties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.CSharp6Features
+{
+	public class AutoProperties
+	{
+		/// <summary>
+		/// We can now declare the initial value of an auto property 
+		/// </summary>
+		public string MyFirstInitialisedAutoProperty { get; set; } = "Woo Hoo";
+
+		/// <summary>
+		/// we can do the same with a  read only auto property
+		/// </summary>
+		public string MyFirstReadOnlyAutoProperty { get; } = "Read Only!";
+
+		/// <summary>
+		/// Expression bodies can be used instead of a get
+		/// </summary>
+		/// <returns></returns>
+		public string MyFirstExpressionBodyMethod() => "I Love C Charp";
+
+		/// <summary>
+		/// This is a candidate for an expression body
+		/// </summary>
+		public string MySecondExpressionBodyMethod
+		{
+			get { return "This COuld be an Expression Body method"; }
+		}
+
+		/// <summary>
+		/// Exprssion body is usefull for concatanting properties
+		/// </summary>
+		public string ExBodyConcatProperties => MyFirstInitialisedAutoProperty + MySecondExpressionBodyMethod;
+
+
+	}
+}
---
+++ CSharp/CSharp6Features/StaticImport.cs
@@ -0,0 +1,19 @@
+﻿using static System.Math;
+
+namespace CSharp.CSharp6Features
+{
+	/// <summary>
+	/// Here we only import the static methods of a class
+	/// </summary>
+	public class StaticImport
+	{
+
+		public void UseStaticMethod()
+		{
+			var result = Sqrt(9);
+
+			//The compiler will convert this to System.Math.Sqrt(9);
+		}
+		
+	}
+}
